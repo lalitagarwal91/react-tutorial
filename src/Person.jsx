bed01e895728d9c1f7a6ff431a68aa8e79917373
lalit.agarwal <lalit.agarwal@thoughtworks.com>
2019-11-27 16:54:34 +0530

    Functional component

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-class Person extends React.Component {
-  render() {
-    console.log(this.props);
-    const { persons } = this.props;
-    const personList = persons.map((person, index) => {
-      console.log(`${index}_${person.id}`);
-      return (
-        <div className="row" key={`${index}_${person.id}`}>
-          <div>My name is {person.name}</div>
-          <div>My age is {person.age}</div>
-        </div>
-      );
-    });
-    return <div>{personList}</div>;
-  }
-}
+const Person = ({ persons }) => {
+  // const Person = props => {
+  // const { persons } = props; //we can directly extract props value in function parameter
+  const personList = persons.map((person, index) => {
+    return (
+      <div className="row" key={`${index}_${person.id}`}>
+        <div>My name is {person.name}</div>
+        <div>My age is {person.age}</div>
+      </div>
+    );
+  });
+  return <div>{personList}</div>;
+};
 export default Person;
