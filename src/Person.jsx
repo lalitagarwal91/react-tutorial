10dab0243e601e0a9604ccf104518b0b27d300f0
lalit.agarwal <lalit.agarwal@thoughtworks.com>
2019-11-27 17:35:14 +0530

    AddPerson component

--- src/Person.jsx
+++ src/Person.jsx
@@ -4,12 +4,22 @@ const Person = ({ persons }) => {
   // const Person = props => {
   // const { persons } = props; //we can directly extract props value in function parameter
   const personList = persons.map((person, index) => {
-    return (
+    // if (person.age > 20) {
+    //   return (
+    //     <div className="row" key={`${index}_${person.id}`}>
+    //       <div>My name is {person.name}</div>
+    //       <div>My age is {person.age}</div>
+    //     </div>
+    //   );
+    // } else {
+    //   return null;
+    // }
+    return person.age > 20 ? ( //in place of if statement we can use ternary operator
       <div className="row" key={`${index}_${person.id}`}>
         <div>My name is {person.name}</div>
         <div>My age is {person.age}</div>
       </div>
-    );
+    ) : null;
   });
   return <div>{personList}</div>;
 };
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Person from "./Person";
+import AddPerson from "./AddPerson";
 class Root extends React.Component {
   state = {
     persons: [
       { name: "Lalit", age: "30", id: "1" },
       { name: "Monika", age: "26", id: "2" },
-      { name: "Shyam", age: "28", id: "3" }
+      { name: "Shyam", age: "20", id: "3" }
     ]
   };
   render() {
     return (
       <div className="container">
         <h1>Welcome, All</h1>
+        <AddPerson />
         <Person persons={this.state.persons} />
       </div>
     );
